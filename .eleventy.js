2ffde716d5ea09ed5a058e03b6a1f2fe7641a821
Walker Mueller <153471572+muellwa@users.noreply.github.com>
2025-11-03 03:35:42 +0000

    new file changes

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,14 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+  
+  
+  eleventyConfig.addShortcode("itemBox", function(imageAlt, imageSrc, description) {
+    return `
+<div class="content-box">
+  <img class="object" alt="${imageAlt}" src="${imageSrc}" width="250" />
+  ${description}
+</div>
+    `;
+  });
 };
--- about.md
+++ about.md
@@ -4,4 +4,3 @@ title: About
 ---
 
 # About the 11ty **Boilerplate**
-<img class="about" alt="pine" src="/images/pine.png" width="50" />
